1882891ae995984f54072f830d3bc1333d31d890
lublubuterbrodi <lublubuterbrodi@gmail.com>
2024-10-08 13:02:36 +0300

    make build

---
+++ index.6610cf6f.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault();var n=t.target.closest("a");n&&(e.src=n.href)});
+//# sourceMappingURL=index.6610cf6f.js.map
